be7c1e142eacdacdfc4642dfe68dff6031296ff6
niolha <olhanikulina2@gmail.com>
2022-05-17 23:23:39 +0200

    first commit

---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <ButtonItem text="Add Task" color="green"/>
+  </header>
+</template>
+
+<script>
+import ButtonItem from "./ButtonItem.vue";
+
+export default {
+  name: "HeaderItem",
+  props: {
+    title: String,
+  },
+  components: {
+    ButtonItem,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/TasksCollection.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div
+      :key="task.id"
+      v-for="task in tasks"
+    >
+      <TaskItem
+        @toggle-reminder="$emit('toggle-reminder', task.id)"
+        @delete-task="$emit('delete-task', task.id)"
+        :task="task"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import TaskItem from "./TaskItem.vue";
+
+export default {
+  name: "TasksCollection",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    TaskItem,
+  },
+  emits: ["delete-task", "toggle-reminder"],
+};
+</script>
